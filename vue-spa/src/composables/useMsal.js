eddca57199ea72e5ea009ec49a2868ccdaf190fc
doughjoe77 <doughjoe77@hotmail.com>
2026-01-21 08:43:28 -0500

    API call is now working

--- vue-spa/src/composables/useMsal.js
+++ vue-spa/src/composables/useMsal.js
@@ -1,7 +1,7 @@
 // src/composables/useMsal.js
 import { ref } from "vue";
 import { msalInstance } from "../msalInstance";
-import { loginRequest } from "../authConfig";
+import { loginRequest, apiRequest } from "../authConfig";
 
 const account = ref(null);
 const accessToken = ref(null);
@@ -12,19 +12,6 @@ export function useMsal() {
     if (accounts.length > 0) {
       account.value = accounts[0];
     }
-  }
-
-  async function acquireToken() {
-    await ensureAccount();
-    if (!account.value) return null;
-
-    const result = await msalInstance.acquireTokenSilent({
-      ...loginRequest,
-      account: account.value
-    });
-
-    accessToken.value = result.accessToken;
-    return result.accessToken;
   }
 
   async function acquireApiToken() {
@@ -42,10 +29,8 @@ export function useMsal() {
   return {
     account,
     accessToken,
-    acquireToken,
     acquireApiToken,
     login: () => msalInstance.loginRedirect(loginRequest),
     logout: () => msalInstance.logoutRedirect()
   };
-
 }
